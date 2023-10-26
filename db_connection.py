a05d52da2dc7399ff7f93cf13a448d70a24d6805
RaulIRC <raulirodriguezcruz@gmail.com>
2023-10-26 11:46:11 -0500

    Added direct references to the variables

--- db_connection.py
+++ db_connection.py
@@ -1,9 +1,9 @@
 import mysql.connector
 import socket
 from functions import databaseErrorMenu
-from settings import database_table
+from settings import database_table, mydb_config
 
-def is_server_online(host, port):
+def is_server_online(host: str, port: str):
     try:
         # Create a socket to the MySQL server
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,18 +16,18 @@ def is_server_online(host, port):
     except Exception as e:
         return False
 
-def initial_connection(db_config):
+def initial_connection(mydb_config: dict [str, ]):
     try:
         # Attempt to connect to the MySQL server
-        conn = mysql.connector.connect(**db_config)
+        conn = mysql.connector.connect(**mydb_config)
         cursor = conn.cursor()
         return conn, cursor
     except mysql.connector.Error as err:
         return None, None
 
-def connect_to_database(db_config, create_if_not_exists=False):
-    if is_server_online(db_config['host'], db_config.get('port', 3306)):
-        conn, cursor = initial_connection(db_config)
+def connect_to_database(mydb_config: dict[str, ], create_if_not_exists=False):
+    if is_server_online(mydb_config['host'], mydb_config.get('port', 3306)):
+        conn, cursor = initial_connection(mydb_config)
         if conn and cursor:
             return conn, cursor
         else:
@@ -37,14 +37,12 @@ def connect_to_database(db_config, create_if_not_exists=False):
                     if databaseErrorMenu():
                         # Try to create the database
                         conn = mysql.connector.connect(
-                            host=db_config['host'],
-                            user=db_config['user'],
-                            password=db_config['password']
+                            host=mydb_config['host'],
+                            user=mydb_config['user'],
+                            password=mydb_config['password']
                         )
                         cursor = conn.cursor()
-                        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_config['database']};")
-                        cursor.execute(f"Use {db_config['database']};")
-                        execute(cursor)
+                        databaseCreation(cursor, mydb_config)
                         conn.commit()
                         conn.close()
                         print("Database created successfully.")
@@ -61,3 +59,8 @@ def execute(cursor):
     cursor.execute(f"CREATE TABLE {database_table}(stdId int PRIMARY KEY Auto_Increment, username varchar(25), password varchar(25), balance int);")
     print("Table Created!")
     return
+
+def databaseCreation(cursor, mydb_config):
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {mydb_config['database']};")
+    cursor.execute(f"Use {mydb_config['database']};")
+    execute(cursor)
